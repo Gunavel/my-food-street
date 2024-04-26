5b86f511ae0033ef49d1c6971247070639e2087c
gunavel <gunavel.bharathi@gmail.com>
2024-04-26 23:28:39 +0530

    add dbclient

---
+++ src/common/utils/dbClient.ts
@@ -0,0 +1,15 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+
+let client: DynamoDBDocument | null = null;
+
+const getClient = () => {
+  if (!client) {
+    const dbClient = new DynamoDB({ endpoint: process.env.DYNAMODB_URL });
+    client = DynamoDBDocument.from(dbClient);
+  }
+
+  return client;
+};
+
+export default getClient;
